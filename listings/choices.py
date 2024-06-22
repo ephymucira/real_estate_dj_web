1f09c6675671f02ac421471ce0e05d4577e750aa
ephymucira <ephymucira@gmail.com>
2024-06-22 16:06:17 +0300

    sdfg

---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+state_choices = {
+
+'Baringo':'Baringo',
+'Bomet':'Bomet',
+'Bungoma':'Bungoma', 
+'Busia':'Busia',
+'Elgeyo Marakwet':'Elgeyo Marakwet', 
+'Embu':'Embu', 
+'Garissa':'Garisa',
+'Homa Bay':'Homa Bay', 
+'Isiolo':'Isiolo', 
+'Kajiado':'Kajiado', 
+'Kakamega':'Kakamega', 
+'Kericho':'Kericho', 
+'Kiambu':'Kiambu', 
+'Kilifi':'Kilifi', 
+'Kirinyaga':'Kirinyaga', 
+'Kisii':'Kisii', 
+'Kisumu':'Kisumu',
+'Kitui':'Kisumu', 
+'Mombasa': 'Mombasa',
+'Nairobi':'Nairobi', 
+}
+bedroom_choices = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10
+}
+
+price_choices = {
+    "100000": 'Kshs.100, 000',
+    "200000": 'Kshs.200, 000',
+    "300000": 'Kshs.300, 000',
+    "400000": 'Kshs.400, 000',
+    "500000": 'Kshs.500, 000',
+    "600000": 'Kshs.600, 000',
+    "700000": 'Kshs.700, 000',
+    "800000": 'Kshs.800, 000',
+    "900000": 'Kshs.900, 000',
+    "1000000": 'Kshs.1M+'
+}
